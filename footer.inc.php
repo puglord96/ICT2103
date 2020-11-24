c566e059227f25fd7c2c9cd8192325ea321ca62e
1902667-JacksonWong <1902667@sit.singaporetech.edu.sg>
2020-11-24 14:02:11 +0800

    update footer page with links

--- footer.inc.php
+++ footer.inc.php
@@ -1,6 +1,8 @@
 <footer class="footer text-center navbar-fixed-bottom" id="footer-custom">
             <nav class="navFooter">
-            <a href=""> Home</a> | <a href=""> Browse</a> | <a href=""> Search</a>
+                <a href="index.php"> Browse</a> | <a href="posting.php"> Posting</a> 
+                | <a href="feedback.php"> Feedback </a>| <a href="DataAnalytics.php"> Data Analytics </a>
+                | <a href="updateinfo.php"> Update Information</a>
             <p> Copyright@ 2020 MySQL-2103</p>
             </nav>
 </footer>
